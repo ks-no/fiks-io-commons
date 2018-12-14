6db58ad9f22a5f39abe1058d3d9b163b4910eb02
Andreas Berre <andreas.berre@webstep.no>
2018-12-14 12:49:13 +0100

    header for dokumentlager id

--- src/main/java/no/ks/fiks/svarinn2/commons/SvarInn2Headers.java
+++ src/main/java/no/ks/fiks/svarinn2/commons/SvarInn2Headers.java
@@ -5,5 +5,6 @@ public final class SvarInn2Headers {
     public static final String MELDING_ID = "melding-id";
     public static final String AVSENDER_NAVN = "avsender-navn";
     public static final String MELDING_TYPE = "type";
+    public static final String DOKUMENTLAGER_ID = "dokumentlager-id";
     public static final String SVAR_PA_MELDING_ID = "svar-til";
 }
