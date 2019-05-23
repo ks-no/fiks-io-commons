a1fa8f7ea136b4fc019a56e35d78c63f1de03686
Idar Borlaug <idar.borlaug@gmail.com>
2019-05-23 10:00:24 +0200

    Oppdatert til 1.2.0

--- src/main/java/no/ks/fiks/io/commons/MeldingsType.java
+++ src/main/java/no/ks/fiks/io/commons/MeldingsType.java
@@ -2,8 +2,4 @@ package no.ks.fiks.io.commons;
 
 public final class MeldingsType {
 
-    public static final String KVITTERING_AVVIST = "no.ks.fiks.kvittering.avvist";
-    public static final String KVITTERING_AKSEPTERT = "no.ks.fiks.kvittering.akseptert";
-    public static final String KVITTERING_FEILET = "no.ks.fiks.kvittering.feilet";
-    public static final String KVITTERING_TIDSAVBRUDD = "no.ks.fiks.kvittering.tidsavbrudd";
 }
