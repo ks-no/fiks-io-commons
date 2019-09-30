0bb3736bbee77c12faad33a4cd3f1c91bc3816a2
Sondre Eikanger Kvalø <zapodot@gmail.com>
2019-09-30 16:24:13 +0200

    Pojos skal kunne serialiseres som JSon mha. Jackson Databind

---
+++ src/test/java/no/ks/fiks/io/commons/MottattMeldingMetadataTest.java
@@ -0,0 +1,33 @@
+package no.ks.fiks.io.commons;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MottattMeldingMetadataTest {
+
+    @DisplayName("Sjekker at klassen er kompatibel med Jackson JSON biblioteket")
+    @Test
+    void jsonSerialization() throws JsonProcessingException {
+        final ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        final MottattMeldingMetadata mottattMeldingMetadata = MottattMeldingMetadata.builder()
+                .avsenderKontoId(UUID.randomUUID())
+                .deliveryTag(Long.MAX_VALUE)
+                .meldingId(UUID.randomUUID())
+                .meldingType("type")
+                .mottakerKontoId(UUID.randomUUID())
+                .ttl(20000L)
+                .svarPaMelding(UUID.randomUUID())
+                .build();
+        final String jsonString = objectMapper.writeValueAsString(mottattMeldingMetadata);
+        assertNotNull(jsonString);
+
+        final MottattMeldingMetadata deserializedObject = objectMapper.readValue(jsonString, MottattMeldingMetadata.class);
+        assertEquals(mottattMeldingMetadata, deserializedObject);
+    }
+}
